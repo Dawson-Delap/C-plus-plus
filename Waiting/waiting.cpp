5fea2571241a81f6cf2161868660ffd5b79606ec
Dawson Delap <dawsondelap97@gmail.com>
2024-10-30 12:16:34 -0700

    updoots

--- Waiting/waiting.cpp
+++ Waiting/waiting.cpp
@@ -11,32 +11,46 @@ int main() {
     char output[50];
     char input[50];
     struct tm datetime;
+    int hour = 0;
+    int min = 0;
+    int sec = 0;
 
+    cout << "Time to shutdown in military time \nHour: ";
+    cin >> hour;
+    cout << "Minute: ";
+    cin >> min;
+    cout << "Second: ";
+    cin >> sec;
     
-    datetime.tm_hour = 14;
-    datetime.tm_min = 33;
-    strftime(input, sizeof(input), "%I:%M %p", &datetime);
+    datetime.tm_hour = hour;
+    datetime.tm_min = min;
+    datetime.tm_sec = sec;
+    strftime(input, sizeof(input), "%I:%M:%S %p", &datetime);
+
 
     while (true) {
-        time_t timestamp = time(NULL);
-        struct tm current_time = *localtime(&timestamp);
-        strftime(output, sizeof(output), "%I:%M %p", &current_time);
-        if (x >= 3600){
-            cout << "You have waited: " << x/3600 << "hours and" << x/3600 % 60 << " minutes and " << x % 60 << " seconds\n";
-        }
-        else if (x >= 60) {
-            cout << "You have waited: " << x / 60 << " minutes and " << x % 60 << " seconds\n";
+        time_t current_time = time(NULL);
+        struct tm *local_current_time = localtime(&current_time);
+        strftime(output, sizeof(output), "%I:%M:%S %p", local_current_time);
+
+        if (x >= 3600) {
+            cout << "\nWaited: " << x / 3600 << " hours and " << x / 60 % 60 << " minutes and " << x % 60 << " seconds\n";
+        } else if (x >= 60) {
+            cout << "\nWaited: " << x / 60 << " minutes and " << x % 60 << " seconds\n";
         } else {
-            cout << "You have waited: " << x << " seconds\n";
+            cout << "\nWaited: " << x << " seconds\n";
         }
+        
         cout << "Current time: " << output << "\n";
+        cout << "Shutting Down at " << input << "";
+
+
 
         if (strcmp(output, input) == 0) {
-            system("c:\\windows\\system32\\shutdown /s");
+            system("shutdown /s /t 0");
             break;
         }
 
-        
         x++;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
--- bignumber/bignumber.cpp
+++ bignumber/bignumber.cpp
@@ -6,10 +6,11 @@
 using namespace std;
 
 int main() {
-    int x = 0;
+    int x = 2;
     for(int i = 1; i>0; ++i){
-        x += 100;
+        x *= 2;
         cout << x <<"\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
     
 }
