ef88585a8ba15826213766897184390be1ef6f3a
Dawson Delap <dawsondelap97@gmail.com>
2024-11-06 17:09:54 -0700

    updoots

--- Slot machine/slots.cpp
+++ Slot machine/slots.cpp
@@ -15,14 +15,25 @@ using namespace std;
 const vector<int> opt = {'0','1','2','3','4','5','6','7','8','9'};
 string board = "|*|*|*|\n| | | |\n|_|_|_|";
 int money = 0;
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 //slot1:9 slot2:11 slot3:13
 int main() { 
+<<<<<<< Updated upstream
     random_device dev;
     mt19937 rng(dev());
     uniform_int_distribution<mt19937::result_type> dist6(0,9); // distribution in range [1, 6]
     int slot1 = dist6(rng); 
     int slot2 = dist6(rng);
     int slot3 = dist6(rng);
+=======
+    srand (time(NULL));
+    int slot1 = rand() % 10; 
+    int slot2 = rand() % 10;
+    int slot3 = rand() % 10;
+>>>>>>> Stashed changes
     board[9] = opt[slot1];
     board[11] = opt[slot2];
     board[13] = opt[slot3];
--- bignumber/bignumber.cpp
+++ bignumber/bignumber.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
     int x = 2;
-    for(int i = 1; i>0; ++i){
+    while(true){
         x += 1010101010;
         cout << x;
     }
